711665214d090feee9c94ced66c2ea670f3c56f5
0xfab1 <f@b.1>
2022-04-21 23:17:54 +0200

    updates

--- os-config/linux/cheatsheets/install.sh
+++ os-config/linux/cheatsheets/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+sudo apt update && sudo apt -y upgrade
 
 # Install Cheat Sheet Tools
-sudo apt update && sudo apt -y upgrade && sudo apt -y
 tldr rlwrap xsel
 tldr -u
 curl https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh
@@ -15,7 +15,7 @@ curl https://cheat.sh/:zsh > ~/.zsh.d/_cht
 echo 'fpath=(~/.zsh.d/ $fpath)' >> ~/.zshrc
 
 # Install Navi
-sudo apt update && sudo apt -y upgrade && sudo apt -y install cargo ftf
+sudo apt -y install cargo ftf
 cargo install --locked navi
 # Config bash
 echo '\n### NAVI ###' >> ~/.bashrc
@@ -27,7 +27,5 @@ echo 'alias navi="~/.cargo/bin/navi"' >> ~/.zshrc
 echo 'eval "\$(navi widget zsh)"' >> ~/.zshrc
 # Install own cheatsheet
 git clone "https://github.com/FullByte/navi-cheatsheet" "$(navi info cheats-path)/FullByte__navi-cheatsheet"
-(crontab -l 2>/dev/null; echo "0 11 * * * bash -c 'cd \"$(navi info cheats-path)/FullByte__navi-cheatsheet\" && /usr/local/bin/git pull -q origin master'") | crontab -
-# Install other cheatsheets
-git clone "https://github.com/infosecstreams/cheat.sheets" "$(navi info cheats-path)/infosecstreams__cheat.sheets"
-(crontab -l 2>/dev/null; echo "0 11 * * * bash -c 'cd \"$(navi info cheats-path)/infosecstreams__cheat.sheets\" && /usr/local/bin/git pull -q origin master'") | crontab -
+(crontab -l 2>/dev/null; echo "@reboot sleep 10 && cd \"$(navi info cheats-path)/FullByte__navi-cheatsheet\" && git pull") | crontab -
+
