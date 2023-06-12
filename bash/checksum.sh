b6da16b572dd65bdcbf65f1410543ec0f2f3b916
0xfab1 <f@b.1>
2023-06-12 17:10:08 +0200

    updates

---
+++ bash/checksum.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#source https://checksum.sh/
+
+function checksum() {
+  local s
+  s=$(curl -fsSL "$1")
+  local h
+  if command -v shasum >/dev/null ; then
+    h=shasum
+  else
+    h=sha1sum
+  fi
+  if [ ! "$2" ] ; then
+    printf %s\\n "$s" | "$h" | awk '{print $1}'
+    return 1;
+  fi
+  printf %s\\n "$s" | "$h" --check --status <(printf '%s  -\n' "$2") || {
+    echo "checksum failed" >&2;
+    return 1;
+  }
+  printf %s\\n "$s"
+}
